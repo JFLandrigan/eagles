bf625e254f23e6bd4efa375c4dacc9c00ce91437
Jon-Frederick Landrigan <jon.landrigan@gmail.com>
2020-08-06 22:20:19 -0400

    plot update

--- eagles/Supervised/utils/tuner_utils.py
+++ eagles/Supervised/utils/tuner_utils.py
@@ -111,7 +111,7 @@ def select_features(
             pu.plot_feature_importance(
                 ft_df=ftImp_df,
                 mod_type=type(forest).__name__,
-                plot_title="Feature Selection Importance",
+                plot_title="RF Feature Selection Importance",
             )
 
     if "regress" in methods:
@@ -145,7 +145,7 @@ def select_features(
             pu.plot_feature_importance(
                 ft_df=tmp,
                 mod_type=type(lin_mod).__name__,
-                plot_title="Feature Selection Importance",
+                plot_title="Logistic l1 Feature Selection Coefs",
             )
 
     # get the final drop and feature sets
@@ -259,7 +259,7 @@ def feature_importances(mod=None, X=None, num_top_fts=None):
             ft_df=ft_imp_df,
             mod_type=type(mod.named_steps["clf"]).__name__,
             num_top_fts=num_top_fts,
-            plot_title="Model Importance",
+            plot_title=type(mod.named_steps["clf"]).__name__ + " Model Importance",
         )
 
     elif type(mod).__name__ == "VotingClassifier":
@@ -286,7 +286,8 @@ def feature_importances(mod=None, X=None, num_top_fts=None):
                     ft_df=tmp_ft_imp_df,
                     mod_type=type(c.named_steps["clf"]).__name__,
                     num_top_fts=num_top_fts,
-                    plot_title="Model Importance",
+                    plot_title=type(mod.named_steps["clf"]).__name__
+                    + " Model Importance",
                 )
             else:
                 tmp_ft_imp_df = get_feature_importances(
@@ -296,7 +297,8 @@ def feature_importances(mod=None, X=None, num_top_fts=None):
                     ft_df=tmp_ft_imp_df,
                     mod_type=type(c).__name__,
                     num_top_fts=num_top_fts,
-                    plot_title="Model Importance",
+                    plot_title=type(mod.named_steps["clf"]).__name__
+                    + " Model Importance",
                 )
 
             tmp_ft_imp_df.columns = ["features", "value"]
@@ -314,7 +316,7 @@ def feature_importances(mod=None, X=None, num_top_fts=None):
             ft_df=ft_imp_df,
             mod_type=type(mod).__name__,
             num_top_fts=num_top_fts,
-            plot_title="Model Importance",
+            plot_title=type(mod.named_steps["clf"]).__name__ + " Model Importance",
         )
 
     return ft_imp_df
