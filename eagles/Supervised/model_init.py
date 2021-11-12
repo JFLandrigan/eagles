6d918fd56edcd11023a413be5cecd22dfb13b861
JFLandrigan <jon.landrigan@gmail.com>
2021-11-11 20:46:58 -0500

    update to skopt

--- eagles/Supervised/model_init.py
+++ eagles/Supervised/model_init.py
@@ -25,7 +25,11 @@ from sklearn.linear_model import (
 from sklearn.svm import SVC, SVR
 from sklearn.neighbors import KNeighborsClassifier, KNeighborsRegressor
 
-from sklearn.impute import SimpleImputer, MissingIndicator, IterativeImputer, KNNImputer
+from sklearn.impute import (
+    SimpleImputer,
+    MissingIndicator,
+    KNNImputer,
+)  # , IterativeImputer
 from sklearn.preprocessing import StandardScaler, MinMaxScaler, RobustScaler
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import KFold, TimeSeriesSplit, StratifiedKFold
